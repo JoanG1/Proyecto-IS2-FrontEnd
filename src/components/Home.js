913fa0379ccf1c8d1643fb98ede69cffc6e45853
JoanGomez <joangomezusma4@gmail.com>
2024-09-28 15:38:23 -0500

    Creacion Home y mensaje de bienvenida

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+// src/components/Home.js
+import React from 'react';
+import './Home.css'; // Para estilos personalizados
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h2>Bienvenido a Harmoniqué</h2>
+            <p>Tu destino para un cuidado de cabello excepcional y estilo elegante.</p>
+        </div>
+    );
+};
+
+export default Home;
